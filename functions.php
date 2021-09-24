1ecb3685c445533413cc9e5efb95335e3396672a
AgEButton <elisabeth.button@ag.tamu.edu>
2021-09-24 09:55:22 -0400

    Theme Files
    Added theme files for Rayden child theme

---
+++ functions.php
@@ -0,0 +1,144 @@
+<?php
+
+// enqueue parent theme styles
+function my_theme_enqueue_styles() {
+
+	$showcase_style = 'rayden-showcase';
+	$hero_style = 'rayden-hero';
+	$portfolio_style = 'rayden-portfolio';
+	$blog_style = 'rayden-blog';
+	$shortcodes_style = 'rayden-shortcodes';
+	$assets_style = 'rayden-assets';
+	$shop_style = 'rayden-shop';
+	$parent_style = 'parent-style';
+	
+	wp_enqueue_style( $showcase_style, get_template_directory_uri() . '/css/showcase.css' );
+	wp_enqueue_style( $hero_style, get_template_directory_uri() . '/css/hero.css' );
+	wp_enqueue_style( $portfolio_style, get_template_directory_uri() . '/css/portfolio.css' );
+	wp_enqueue_style( $blog_style, get_template_directory_uri() . '/css/blog.css' );
+	wp_enqueue_style( $shortcodes_style, get_template_directory_uri() . '/css/shortcodes.css' );
+	wp_enqueue_style( $assets_style, get_template_directory_uri() . '/css/assets.css' );
+	wp_enqueue_style( $shop_style, get_template_directory_uri() . '/css/shop.css' );
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	
+	// if WpBakery is installed, make sure we enqueue js composer syles and library 
+	if ( function_exists( 'vc_set_as_theme' ) ) {
+		
+		// the child script childscript.js will call vc_js() on ajax success event
+		wp_enqueue_script( 'wpb_composer_front_js' );
+
+		wp_enqueue_style( 'js_composer_front' );
+		wp_enqueue_style( 'js_composer_custom_css' );
+		
+		if( !function_exists( 'filter_the_content_vc_custom_styles' ) ){
+			
+			// retrieve vc custom CSS styles added for each row
+			function filter_the_content_vc_custom_styles( $content ) {
+		
+				if ( isset( $_GET['preview'] ) && 'true' === $_GET['preview'] ) {
+					 
+					 return $content;
+				}
+				
+				/* AJAX check  */
+				if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+					
+					$this_post_id = null;
+				
+					// Check if we're inside the main loop in a single post page.
+					if ( ( is_single() || is_page() ) && in_the_loop() &&  is_main_query() ) {
+						
+						if ( is_front_page() || is_home() ) {
+							$this_post_id = get_queried_object_id();
+						} else if ( is_singular() ) {
+							if ( !$this_post_id ) {
+								$this_post_id = get_the_ID();
+							}
+						}
+
+						$post_custom_css = get_metadata( 'post', $this_post_id, '_wpb_shortcodes_custom_css', true );
+						if ( ! empty( $post_custom_css ) ) {
+							
+							$post_custom_css = strip_tags( $post_custom_css );
+							return $content . '<style type="text/css" data-type="vc_custom-css">' . $post_custom_css . '</style>';
+						}
+						
+					}
+				}
+				
+				return $content;
+			}
+		}
+		add_filter( 'the_content', 'filter_the_content_vc_custom_styles' );
+	}
+	
+	// register your child script
+	wp_register_script( 'childscript', get_stylesheet_directory_uri() . '/childscript.js', array('jquery'), false, true );
+	wp_enqueue_script( 'childscript' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// allowable HTML tags in theme options - add here more than the ones already accepted by the theme
+if( !function_exists('rayden_child_kses_allowed_html') ){
+
+	function rayden_child_kses_allowed_html($tags, $context) {
+		
+		switch($context) {
+			case 'rayden_allowed_html':
+				$tags = array(
+					'a' => array(
+								'id' => array(),
+								'href' => array(),
+								'title' => array(),
+								'rel' => array(),
+								'target' => array(),
+								'class' => array(),
+								'data-type' => array(),
+								'data-filter' => array()
+							),
+					'div' => array(
+								'id' => array(),
+								'class' => array()
+								),
+					'span' => array(
+								'id' => array(),
+								'class' => array(),
+								'data-hover' => array()
+								),
+					'img' => array(
+								'src' => array(),
+								'alt' => array(),
+								'width' => array(),
+								'height' => array(),
+								'class' => array()
+								),
+					'h1' => array(),
+					'h2' => array(),
+					'h3' => array(),
+					'h4' => array(),
+					'h5' => array(),
+					'h6' => array(),
+					'ul' => array(),
+					'li' => array(),
+					'br' => array(),
+					'em' => array(),
+					'strong' => array(),
+					'b' => array(),
+					'i' => array(),
+					'u' => array(),
+					'p' => array(),
+					'hr' => array(),
+					'figure' => array()
+				);
+				return $tags;
+			default: 
+				return $tags;
+		}
+
+	}
+
+	add_filter( 'wp_kses_allowed_html', 'rayden_child_kses_allowed_html', 9, 2 );
+
+}
+
+?>
